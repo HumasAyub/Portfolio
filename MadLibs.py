44de903605943db7c13fa1bfcd7e1765a83fc2cd
HumasAyub <111377281+HumasAyub@users.noreply.github.com>
2022-08-17 14:49:14 +0100

    Add files via upload

---
+++ MadLibs.py
@@ -0,0 +1,6 @@
+Colour = input("Insert name of colour here: ")
+PluralNoun = input("Insert name of PluralNoun here: ")
+Celebrity = input("Insert name of Celebrity here: ")
+
+print("Roses are "+ Colour +", " + PluralNoun +" are blue, I love " + Celebrity)
+
